1378edeb8cafbcfaf0d9773734d2983ed6b132f2
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-08-10 15:07:19 +0530

    Add files via upload

--- views/index.js
+++ views/index.js
@@ -1,35 +1,31 @@
 import express from 'express'
-import {dirname} from 'path'
-import {fileURLToPath} from 'url'
-import bodyParser from 'body-parser'
+
+
 const app = express()
+const port = 3000
 
-const port = 3001
-const _dirname = dirname(fileURLToPath(import.meta.url))
-var bandname
-app.get("/", (req, res)=>{
-
-    res.sendFile(_dirname+"/index.html")
+app.listen(port, ()=>{
+    console.log("listening to port "+ port)
 })
 
-app.use(bodyParser.urlencoded({extended:true}))
+const day_select = new Date().getDay()
+
+
+app.get("/", (req,res)=>{
+
+    
+})
 
 
 
-function test (req, res, next){
+function check(){
+    if (day_select>=1 || day_select<=6){
+        console.log("weekday")
+    }
 
-    console.log(req.body)
-    bandname = req.body["email"] + req.body["password"]
-    next()
+    else{
+        console.log("weekend")
+    }
 }
 
-
-app.use(test)
-
-app.post("/submit", (req,res)=>{
-    res.send(`<h1>Your band name generator is </h1> <h2> ${bandname} </h2>`)
-})
-app.listen(port,()=>{
-    console.log(`listenin on port ${port}`)
-})
-
+check()
