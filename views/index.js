abf3114185bfbd3b427fcb263081dcac80420f93
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-08-10 15:18:16 +0530

    Add files via upload

--- views/index.js
+++ views/index.js
@@ -13,19 +13,23 @@ const day_select = new Date().getDay()
 
 app.get("/", (req,res)=>{
 
+    let day = ""
+    let advice = ""
+    function check(){
+        if (day_select>=1 || day_select<=6){
+            day = "weekday"
+            advice = "Work hard"
+        }
     
+        else{
+            day = "weekend"
+            advice = "Party"
+        }
+    }
+    
+    check()
+
 })
 
 
 
-function check(){
-    if (day_select>=1 || day_select<=6){
-        console.log("weekday")
-    }
-
-    else{
-        console.log("weekend")
-    }
-}
-
-check()
